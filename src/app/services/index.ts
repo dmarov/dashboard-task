2b431713b202632bce254e420761c766e10c00cb
Dmitry Marov <d.marov94@gmail.com>
2022-04-23 20:05:51 +0300

    added service

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,3 +1,4 @@
 export * from './posts';
 export * from './albums';
 export * from './photos';
+export * from './users';
--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -1,25 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Constants } from "@/core";
-import { ApiPost } from "@/models";
+import { ApiUser } from "@/models";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class PostsService {
+export class UsersService {
 
     constructor(
         private readonly $http: HttpClient,
     ) { }
 
-    getPosts(): Observable<ApiPost[]> {
-        return this.$http.get<ApiPost[]>(
-            `${Constants.apiBase}/posts`
-        );
-    }
-
-    getPost(id: number): Observable<ApiPost> {
-        return this.$http.get<ApiPost>(
-            `${Constants.apiBase}/posts/${id}`
+    getUser(id: number): Observable<ApiUser> {
+        return this.$http.get<ApiUser>(
+            `${Constants.apiBase}/users/${id}`
         );
     }
 }
