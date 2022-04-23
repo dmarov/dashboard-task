87a58976843982de54a71a3469e5857299027f87
Dmitry Marov <d.marov94@gmail.com>
2022-04-23 19:29:21 +0300

    photos wip

--- src/app/modules/photos.module.ts
+++ src/app/modules/photos.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import {
     PagePhotosComponent,
 } from '@/components';
-import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageModule } from '@/modules/layout-page.module';
 import * as PhotosReducers from '@/store/reducers/photos';
 import { PhotosService } from '@/services';
 import { PhotosEffects } from '@/store/effects';
+import { PhotoComponentsModule } from '@/modules/photo-components.module';
 
 const routes: Routes = [
     { path: '', component: PagePhotosComponent },
@@ -20,10 +22,12 @@ const routes: Routes = [
         PagePhotosComponent,
     ],
     imports: [
+        HttpClientModule,
         RouterModule.forChild(routes),
         LayoutPageModule,
         StoreModule.forFeature(PhotosReducers.featureKey, PhotosReducers.reducer),
         EffectsModule.forFeature([ PhotosEffects ]),
+        PhotoComponentsModule,
     ],
     providers: [
         PhotosService,
