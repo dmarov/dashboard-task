f78f14707804b6b97cbbd74aa018ee0bcbe3a6fd
Dmitry Marov <d.marov94@gmail.com>
2022-04-24 19:50:05 +0300

    breadcrumbs wip

--- src/app/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -10,7 +10,18 @@ export class BreadcrumbsComponent implements OnInit {
     @HostBinding('class.breadcrumbs')
     blockClass = true;
 
+    paths: string[] = [];
+
     ngOnInit(): void {
         const path = window.location.pathname;
+        const parts = path.split('/').map(p => p.replace('/', ''));
+        let cumulative = '';
+
+        for (const part of parts) {
+            if (part) {
+                cumulative += '/' + part;
+                this.paths.push(cumulative);
+            }
+        }
     }
 }
--- src/app/modules/breadcrumbs.module.ts
+++ src/app/modules/breadcrumbs.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import {
     BreadcrumbsComponent,
@@ -7,6 +9,10 @@ import {
 @NgModule({
     declarations: [
         BreadcrumbsComponent,
+    ],
+    imports: [
+        RouterModule,
+        CommonModule,
     ],
     exports: [
         BreadcrumbsComponent,
