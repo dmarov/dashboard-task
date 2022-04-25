46ca6756f96fc11c123c9fd463dbe46ef7f5e8be
Dmitry Marov <d.marov94@gmail.com>
2022-04-25 14:09:14 +0300

    tests wip

--- src/app/components/page-photo/page-photo.component.spec.ts
+++ src/app/components/page-photo/page-photo.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+import { StoreBaseModule } from '@/modules/store-base.module';
 
 import { PagePhotoComponent } from './page-photo.component';
 
@@ -8,7 +10,11 @@ describe('PagePhotoComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ PagePhotoComponent ]
+      declarations: [ PagePhotoComponent ],
+      imports: [
+        RouterTestingModule,
+        StoreBaseModule,
+      ],
     })
     .compileComponents();
   }));
