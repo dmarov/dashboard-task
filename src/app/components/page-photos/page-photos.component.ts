74f8f385bc82eab58497ff1caba7d3d61c021694
Dmitry Marov <d.marov94@gmail.com>
2022-05-09 23:14:44 +0300

    refactored query parser

--- src/app/components/page-photos/page-photos.component.ts
+++ src/app/components/page-photos/page-photos.component.ts
@@ -4,9 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { PhotosActions } from '@/store/actions';
-import { PhotosSelectors } from '@/store/selectors';
-import { withLatestFrom } from 'rxjs/operators';
-import { QueryParser } from '@/utils';
+import { PhotosSelectors, RouterSelectors } from '@/store/selectors';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
     selector: 'app-page-photos',
@@ -67,14 +66,44 @@ export class PagePhotosComponent implements OnInit, OnDestroy {
         );
 
         this.subscription.add(
-            this.activatedRoute
-                .queryParamMap.pipe(
-                    withLatestFrom(
-                        this.activePage$,
-                        this.searchTerm$,
-                        this.searchField$,
-                    )
-                ).subscribe(this.updateState)
+            this.store$.pipe(
+                select(RouterSelectors.selectPage),
+                distinctUntilChanged(),
+            ).subscribe(page => {
+                this.store$.dispatch(
+                    PhotosActions.setActivePage({ page })
+                );
+            })
+        );
+
+        this.subscription.add(
+            this.store$.pipe(
+                select(RouterSelectors.selectSearchTerm),
+                distinctUntilChanged(),
+            ).subscribe(term => {
+                this.store$.dispatch(
+                    PhotosActions.setSearchTerm({ term })
+                );
+
+                this.store$.dispatch(
+                    PhotosActions.setActivePage({ page: 0 })
+                );
+            })
+        );
+
+        this.subscription.add(
+            this.store$.pipe(
+                select(RouterSelectors.selectSearchField),
+                distinctUntilChanged(),
+            ).subscribe(field => {
+                this.store$.dispatch(
+                    PhotosActions.setSearchField({ field })
+                );
+
+                this.store$.dispatch(
+                    PhotosActions.setActivePage({ page: 0 })
+                );
+            })
         );
     }
 
@@ -100,54 +129,6 @@ export class PagePhotosComponent implements OnInit, OnDestroy {
             queryParams: { searchField: field },
             queryParamsHandling: 'merge',
         });
-    }
-
-    updateState = ([params, prevPage, prevTerm, prevField]) => {
-        const page = QueryParser.parsePage(params);
-        const term = QueryParser.parseSearch(params);
-        const field = QueryParser.parseSearchField(params);
-
-        this.updatePage(page, prevPage);
-        this.updateTerm(term, prevTerm);
-        this.updateField(field, prevField);
-    }
-
-    updatePage(page: number, prevPage: number) {
-        const pageMismatch = page !== prevPage;
-
-        if (pageMismatch) {
-            this.store$.dispatch(
-                PhotosActions.setActivePage({ page })
-            );
-        }
-    }
-
-    updateTerm(term: string, prevTerm: string) {
-        const termMismatch = term !== prevTerm;
-
-        if (termMismatch) {
-            this.store$.dispatch(
-                PhotosActions.setSearchTerm({ term })
-            );
-
-            this.store$.dispatch(
-                PhotosActions.setActivePage({ page: 0 })
-            );
-        }
-    }
-
-    updateField(field: PhotosSearchFieldType, prevField: PhotosSearchFieldType) {
-        const fieldMismatch = field !== prevField;
-
-        if (fieldMismatch) {
-            this.store$.dispatch(
-                PhotosActions.setSearchField({ field })
-            );
-
-            this.store$.dispatch(
-                PhotosActions.setActivePage({ page: 0 })
-            );
-        }
     }
 
     ngOnDestroy() {
